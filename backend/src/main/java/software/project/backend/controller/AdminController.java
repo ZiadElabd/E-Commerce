8e86758627ac35621475b3cf25d3284ed7c0946d
ziadalbana <58531158+ziadalbana@users.noreply.github.com>
2022-01-02 23:59:31 +0200

    admin requests

--- backend/src/main/java/software/project/backend/controller/AdminController.java
+++ backend/src/main/java/software/project/backend/controller/AdminController.java
@@ -3,29 +3,51 @@ package software.project.backend.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import software.project.backend.Model.Product;
+import software.project.backend.sercuirty.Singelton;
 import software.project.backend.service.adminService;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/admin")
 public class AdminController {
-    adminService service=new adminService();
+    private adminService service=new adminService();
+    private Singelton trackingSystem=Singelton.getInstance();
     @PostMapping("/addProduct")
-    public ResponseEntity<Boolean> addProductController(@RequestBody String temp){
-        if (service.addProduct(temp)) return new ResponseEntity<>(true, HttpStatus.OK);
+    public ResponseEntity<Boolean> addProductController(@RequestBody String temp,
+                                                        @PathVariable("ID") String seesionID){
+        if (service.addProduct(seesionID,temp)) return new ResponseEntity<>(true, HttpStatus.OK);
         return new ResponseEntity<>(false, HttpStatus.FORBIDDEN);
     }
-    @PutMapping("/updateProduct/{productID}")
+    @PutMapping("/updateProduct/{productID}/{ID}")
     public ResponseEntity<Boolean> updateProductController(@PathVariable("productID") int productID,
-                                                           @RequestBody String temp){
-        if (service.UpdateProduct(productID,temp)) return new ResponseEntity<>(true, HttpStatus.OK);
+                                                           @RequestBody String temp,
+                                                           @PathVariable("ID") String seesionID){
+        if (service.UpdateProduct(seesionID,productID,temp)) return new ResponseEntity<>(true, HttpStatus.OK);
         return new ResponseEntity<>(false, HttpStatus.FORBIDDEN);
     }
-    @DeleteMapping ("/{productID}")
-    public ResponseEntity<Boolean> deleteProduct(@PathVariable("productID") String temp){
+    @DeleteMapping ("/{productID}/{ID}")
+    public ResponseEntity<Boolean> deleteProduct(@PathVariable("productID") String temp,
+                                                 @PathVariable("ID") String seesionID){
         System.out.println(temp);
-        if(service.deleteProduct(temp)) return new ResponseEntity<>(true, HttpStatus.ACCEPTED);
+        if(service.deleteProduct(seesionID,temp)) return new ResponseEntity<>(true, HttpStatus.ACCEPTED);
         return new ResponseEntity<>(false, HttpStatus.FORBIDDEN);
+    }
+    @GetMapping("/getProducts/{categroyName}/{ID}")
+    public ResponseEntity<List<Product>> getProductsByCategroy(@PathVariable("categroyName") String categroyName,
+                                                               @PathVariable("ID") String seesionID){
+        List<Product> ProductsByCategroy=service.getAllProductByCategroy(seesionID,categroyName);
+        if (ProductsByCategroy!=null) return  new ResponseEntity<>(ProductsByCategroy, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
+    }
+    @GetMapping("/getProduct/{productID}/{ID}")
+    public ResponseEntity<Product> getProduct(@PathVariable("productID") int productID,
+                                              @PathVariable("ID") String seesionID){
+        Product result=service.getProductByID(seesionID,productID);
+        if (result!=null) return  new ResponseEntity<>(result, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
     }
 
 }
--- backend/src/main/java/software/project/backend/service/adminService.java
+++ backend/src/main/java/software/project/backend/service/adminService.java
@@ -5,21 +5,43 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import software.project.backend.Database.ProductDAO;
 import software.project.backend.Model.Product;
 import software.project.backend.Model.builder.Director;
+import software.project.backend.sercuirty.Singelton;
+
+import java.util.List;
 
 public class adminService {
     private Director director=new Director();
     @Autowired
     private JdbcTemplate jdbcTemplate=new JdbcTemplate();
     private ProductDAO productOperation=new ProductDAO();
-    public boolean addProduct(String dataSent){
+    private Singelton trackingSystem=Singelton.getInstance();
+    public boolean addProduct(String sessionID,String dataSent){
+        String userName=trackingSystem.checkAcess(sessionID);
+        if (userName==null) return false;
         Product product=(Product) director.composeModel("product",dataSent);
         return productOperation.insertProduct(product);
     }
-    public boolean UpdateProduct(int productID,String dataSent){
+    public boolean UpdateProduct(String sessionID,int productID,String dataSent){
+        String userName=trackingSystem.checkAcess(sessionID);
+        if (userName==null) return false;
         Product product=(Product) director.composeModel("product",dataSent);
         return productOperation.updateProduct(product,productID);
     }
-    public boolean deleteProduct(String name){
-        return productOperation.deleteProduct(name);
+    public boolean deleteProduct(String sessionID,String productID){
+        String userName=trackingSystem.checkAcess(sessionID);
+        if (userName==null) return false;
+        return productOperation.deleteProduct(productID);
     }
+
+    public List<Product> getAllProductByCategroy(String sessionID,String categroy){
+        String userName=trackingSystem.checkAcess(sessionID);
+        if (userName==null) return null;
+        return productOperation.getProductByCategory(categroy);
+    }
+    public Product getProductByID(String sessionID,int productID){
+        String userName=trackingSystem.checkAcess(sessionID);
+        if (userName==null) return null;
+        return productOperation.getProductByID(productID);
+    }
+
 }

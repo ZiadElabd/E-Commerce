74a9040b991f526c290f1e3e794afe3f114336ec
ziadalbana <58531158+ziadalbana@users.noreply.github.com>
2022-02-08 19:56:46 +0200

    cart operation added+search back

--- backend/src/main/java/software/project/backend/controller/UserController.java
+++ backend/src/main/java/software/project/backend/controller/UserController.java
@@ -3,11 +3,61 @@ package software.project.backend.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import software.project.backend.Model.Product;
+import software.project.backend.sercuirty.Singelton;
 import software.project.backend.service.userService;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/user")
 public class UserController {
+    private userService service=new userService();
+    private Singelton trackingSystem=Singelton.getInstance();
+    @PostMapping("search/{ID}")
+    public ResponseEntity<List<Product>> search(@PathVariable("ID") String sessionID,
+                                                @RequestBody String temp){
+        System.out.println(temp);
+        List<Product> products=service.search(sessionID,temp);
+        if(products!=null) return  new ResponseEntity<>(products, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
+    }
+    @PostMapping("addToCart/{ID}")
+    public ResponseEntity<Boolean> addToCart(@PathVariable("ID") String sessionID,
+                                             @RequestBody String temp){
+        System.out.println(temp);
+        if(service.addToCart(sessionID,temp))  return new ResponseEntity<>(true, HttpStatus.OK);
+        return new ResponseEntity<>(false, HttpStatus.FORBIDDEN);
+    }
+    @DeleteMapping("deleteFromCart/{ID}/{productID}")
+    public ResponseEntity<Boolean> deleteFromCart(@PathVariable("ID") String sessionID,
+                                                  @PathVariable("productID") int productID){
+        if(service.deleteFromCart(sessionID,productID))  return new ResponseEntity<>(true, HttpStatus.OK);
+        return new ResponseEntity<>(false, HttpStatus.FORBIDDEN);
+    }
+    @DeleteMapping("deleteCart/{ID}")
+    public ResponseEntity<Boolean> deleteCart(@PathVariable("ID") String sessionID){
+        if(service.deleteCart(sessionID))  return new ResponseEntity<>(true, HttpStatus.OK);
+        return new ResponseEntity<>(false, HttpStatus.FORBIDDEN);
+    }
+    @GetMapping("/getCart/{ID}")
+    public ResponseEntity<List<Product>> getCart(@PathVariable("ID") String sessionID){
+        List<Product> products=service.getCart(sessionID);
+        if(products!=null) return  new ResponseEntity<>(products, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
+    }
+    @GetMapping("/checkCart/{ID}/{productID}")
+    public ResponseEntity<Boolean> checkCart(@PathVariable("ID") String sessionID,
+                                             @PathVariable("productID") int productID){
+        if(service.checkCart(sessionID,productID)) return  new ResponseEntity<>(true, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(false, HttpStatus.FORBIDDEN);
+    }
+    @PutMapping("updateCart/{ID}")
+    public ResponseEntity<Boolean> updateCart(@PathVariable("ID") String sessionID,
+                                              @RequestBody String temp){
+        if(service.updateCart(sessionID,temp)) return  new ResponseEntity<>(true, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(false, HttpStatus.FORBIDDEN);
+    }
 
 }
--- backend/src/main/java/software/project/backend/service/userService.java
+++ backend/src/main/java/software/project/backend/service/userService.java
@@ -4,12 +4,84 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import software.project.backend.Database.ProductDAO;
 import software.project.backend.Database.UserDAO;
+import software.project.backend.Model.Product;
 import software.project.backend.Model.User;
 import software.project.backend.Model.builder.Director;
 import software.project.backend.sercuirty.Singelton;
 import software.project.backend.sercuirty.passwordOperations;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 public class userService {
+    private Director director=new Director();
+    private ProductDAO productDAO=new ProductDAO();
+    private Singelton trackingSystem=Singelton.getInstance();
+    private passwordOperations passwordOperations=new passwordOperations();
+    public List<Product> search(String sessionID,String dataSent){
+        String userName=trackingSystem.checkAcess(sessionID);
+        if (userName==null) return null;
+        String cateogry="";
+        String searchText="";
+        try {
+            JSONObject obj = new JSONObject(dataSent);
+            cateogry=obj.getString("categoryName");
+            searchText=obj.getString("searchText");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return productDAO.searchWithName(searchText,cateogry);
+    }
+    public boolean addToCart(String sessionID,String dataSent){
+        String userName=trackingSystem.checkAcess(sessionID);
+        if (userName==null) return false;
+        int productID = 0;
+        int noOfCopies = 0;
+        try {
+            JSONObject obj = new JSONObject(dataSent);
+            productID=obj.getInt("productID");
+            noOfCopies=obj.getInt("noOfCopies");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return productDAO.insertToCart(userName,productID,noOfCopies);
+    }
+    public boolean deleteFromCart(String sessionID,int productID ){
+        String userName=trackingSystem.checkAcess(sessionID);
+        if (userName==null) return false;
+        return productDAO.deleteFromCart(productID,userName);
+    }
+    public boolean deleteCart(String sessionID){
+        String userName=trackingSystem.checkAcess(sessionID);
+        if (userName==null) return false;
+        return productDAO.clearCart(userName);
+    }
+    public List<Product> getCart(String sessionID){
+        String userName=trackingSystem.checkAcess(sessionID);
+        if (userName==null) return null;
+        return null;
+    }
+    public boolean updateCart(String sessionID,String dataSent){
+        String userName=trackingSystem.checkAcess(sessionID);
+        if (userName==null) return false;
+        int productID = 0;
+        int noOfCopies = 0;
+        try {
+            JSONObject obj = new JSONObject(dataSent);
+            productID=obj.getInt("productID");
+            noOfCopies=obj.getInt("noOfCopies");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return productDAO.updateCart(userName,productID,noOfCopies);
+    }
+   public  boolean checkCart(String sessionID,int productID){
+       String userName=trackingSystem.checkAcess(sessionID);
+       if (userName==null) return false;
+       return productDAO.isInCart(userName,productID);
+   }
 
 }
